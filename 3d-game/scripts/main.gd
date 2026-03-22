842466c30adbb17d67f55dd92df5c3a7627407e3
fishygaminglife-del <fishygaming.life@gmail.com>
2026-03-21 19:21:15 -0500

    Door Animation
    I animated the door, so that the it starts closed (you can not enter room due to collision body) and the door and collision body animate and open so player can enter room.

--- 3d-game/scripts/main.gd
+++ 3d-game/scripts/main.gd
@@ -3,9 +3,13 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimationPlayer/Text.text = "Welcome to my first 3D Game"
-	$AnimationPlayer.play("textplay")
-	await $AnimationPlayer.animation_finished
-	$AnimationPlayer/Textbox.visible = false
-	$AnimationPlayer/Name.visible = false
-	$AnimationPlayer/Text.visible = false
+	$Building/StaticCollision/CollisionShape3D6.position = Vector3(-6, 1.5, 11.8)
+	$Building/StaticCollision.rotation_degrees = Vector3(0, 0, 0)
+	$TextPlayer/Text.text = "Welcome to my first 3D Game"
+	$TextPlayer.play("textplay")
+	await $TextPlayer.animation_finished
+	$TextPlayer/Textbox.visible = false
+	$TextPlayer/Name.visible = false
+	$TextPlayer/Text.visible = false
+	$MainScene.play("door_open")
+	await $MainScene.animation_finished
