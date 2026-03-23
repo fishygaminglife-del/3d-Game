281d0b741089f6379bd6b001345afc251d535e79
fishygaminglife-del <fishygaming.life@gmail.com>
2026-03-23 18:18:39 -0500

    Finished torch puzzle
    I finished the torch puzzle, so now the user has to play the torch puzzle and cool animation comes.

--- 3d-game/scripts/puzzle_2.gd
+++ 3d-game/scripts/puzzle_2.gd
@@ -3,8 +3,19 @@ extends Node3D
 
 var cur_torch = 0
 var cur_code = ""
+var correct_order = [1, 2, 3, 4]
+var code_shuffled = ""
+var code_input = []
+var t_1 = 0
+var t_2 = 0
+var t_3 = 0
+var t_4 = 0
 
 func _ready() -> void:
+	correct_order.shuffle()
+	code_shuffled =  "".join(correct_order.map(str))
+	print(code_shuffled)
+	$CodeWall.text = code_shuffled
 	$StaticBody3D/OmniLight3D2.visible = false
 	$StaticBody3D2/OmniLight3D2.visible = false
 	$StaticBody3D3/OmniLight3D2.visible = false
@@ -17,43 +28,71 @@ func _input(event):
 	if event.is_action_pressed("interact") and Global.can_torch == true:
 		if cur_torch == 1:
 			$StaticBody3D/OmniLight3D2.visible = true
+			add_number(1)
 		elif cur_torch == 2:
 			$StaticBody3D2/OmniLight3D2.visible = true
+			add_number(2)
 		elif cur_torch == 3:
 			$StaticBody3D3/OmniLight3D2.visible = true
+			add_number(3)
 		elif cur_torch == 4:
 			$StaticBody3D4/OmniLight3D2.visible = true
-		
-func _process(delta: float) -> void:
-	pass
-
-
+			add_number(4)
+func add_number(num):
+	code_input.append(num)
+	if code_input.size() == 4:
+		if code_input == correct_order:
+			print("correct")
+			$"../MainScene".play("code_correct")
+			await $"../MainScene".animation_finished
+		else:
+			$StaticBody3D/OmniLight3D2.visible = false
+			$StaticBody3D2/OmniLight3D2.visible = false
+			$StaticBody3D3/OmniLight3D2.visible = false
+			$StaticBody3D4/OmniLight3D2.visible = false
+			$"../ProtoController".position = Vector3(-2.2, 3, -16.7)
+			code_input.clear()
+			cur_torch = 0
+			t_1 = 0
+			t_2 = 0
+			t_3 = 0
+			t_4 = 0
+			
 func _on_triger_t_1_body_entered(body: Node3D) -> void:
-	cur_torch = 1
-	$TrigerT1/Torch1.visible = true
-
+	if t_1 == 0:
+		t_1 = 1
+		cur_torch = 1
+		$TrigerT1/Torch1.visible = true
+	
 func _on_triger_t_1_body_exited(body: Node3D) -> void:
-	$TrigerT1/Torch1.visible = false
-	cur_torch = 0
+		$TrigerT1/Torch1.visible = false
+		cur_torch = 0
 
 func _on_triger_t_2_body_entered(body: Node3D) -> void:
-	cur_torch = 2
-	$TrigerT2/Torch2.visible = true
+	if t_2 == 0:
+		t_2 = 1
+		cur_torch = 2
+		$TrigerT2/Torch2.visible = true
 
 func _on_triger_t_2_body_exited(body: Node3D) -> void:
 	cur_torch = 0
 	$TrigerT2/Torch2.visible = false
+
 func _on_triger_t_3_body_entered(body: Node3D) -> void:
-	cur_torch = 3
-	$TrigerT3/Torch3.visible = true
+	if t_3 == 0:
+		t_3 = 1
+		cur_torch = 3
+		$TrigerT3/Torch3.visible = true
 
 func _on_triger_t_3_body_exited(body: Node3D) -> void:
 	cur_torch = 0
 	$TrigerT3/Torch3.visible = false
 
 func _on_triger_t_4_body_entered(body: Node3D) -> void:
-	cur_torch = 4
-	$TrigerT4/Torch4.visible = true
+	if t_4 == 0:
+		t_4 = 1
+		cur_torch = 4
+		$TrigerT4/Torch4.visible = true
 
 
 func _on_triger_t_4_body_exited(body: Node3D) -> void:
